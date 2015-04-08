37e0b9be87d2e0ec9e54cd93a865dcffe9f13d6e
danny-gonz <dpg2012@hotmail.com>
2015-04-08 15:48:34 -0700

    intial

---
+++ PoliConnect/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PoliConnect.Startup))]
+namespace PoliConnect
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
